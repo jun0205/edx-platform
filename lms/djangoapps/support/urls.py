576102a97c77ea186e7c06fbd8dc6dc8529843f2
McKenzie Welter <mwelter@edx.org>
2018-03-07 12:30:12 -0500

    move support actions to existing entitlement api

--- lms/djangoapps/support/urls.py
+++ lms/djangoapps/support/urls.py
@@ -5,27 +5,16 @@ from django.conf.urls import url
 
 from lms.djangoapps.support.views.contact_us import ContactUsView
 from support.views.certificate import CertificatesSupportView
-from support.views.course_entitlements import EntitlementSupportView
 from support.views.enrollments import EnrollmentSupportListView, EnrollmentSupportView
 from support.views.index import index
 from support.views.manage_user import ManageUserDetailView, ManageUserSupportView
 from support.views.refund import RefundSupportView
 
-COURSE_ENTITLEMENTS_VIEW = EntitlementSupportView.as_view({
-    'get': 'list',
-    'post': 'create',
-    'put': 'update'
-})
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^certificates/?$', CertificatesSupportView.as_view(), name="certificates"),
     url(r'^refund/?$', RefundSupportView.as_view(), name="refund"),
-    url(
-        r'^course_entitlement/(?P<username_or_email>[\w.@+-]+)?$',
-        COURSE_ENTITLEMENTS_VIEW,
-        name="course_entitlement"
-    ),
     url(r'^enrollment/?$', EnrollmentSupportView.as_view(), name="enrollment"),
     url(r'^contact_us/?$', ContactUsView.as_view(), name="contact_us"),
     url(
